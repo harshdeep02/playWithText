ce46450eb85717e099fa2cf13b584951771b2170
Harsh Deep <harshdeep7887@gmail.com>
2023-07-14 11:38:23 +0530

    update components

--- src/components/Textinp.js
+++ src/components/Textinp.js
@@ -61,6 +61,11 @@ export default function Textinp(props) {
   <textarea className='form-control' rows="8" style ={{backgroundColor:props.mode==='light'?'#fff':'#607d8b', color:props.mode==='light'?'#000000':'#fff'}} value={Text} id="myBox" onChange={handleOnChange} aria-label="With textarea"></textarea>
 </div>
 
+
+
+
+
+
 <button disabled ={Text.length === 0} className="btn btn-primary mx-2" onClick={handleUpperCase}>To Uppercase</button>
 <button disabled ={Text.length === 0}className="btn btn-primary mx-2" onClick={handleloCase}>To Lowercase</button>
 <button disabled ={Text.length === 0}className="btn btn-primary mx-2" onClick={cleartext}>Clear</button>
@@ -72,7 +77,7 @@ export default function Textinp(props) {
 </div>
   <div className="container">
     <h2 className="my-3" style={{color:props.mode==='light'?'#000000':'#fff'}}>Your text summary</h2>
-    <p style={{color:props.mode==='light'?'#000000':'#fff'}}>{Text.split(" ").filter((element)=>{return element.length!==0}).length} words and {Text.length} characters</p>
+    <p style={{color:props.mode==='light'?'#000000':'#fff'}}>{Text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {Text.length} characters</p>
     <p style={{color:props.mode==='light'?'#000000':'#fff'}}>{0.008 * Text.split(" ").filter((element)=>{return element.length!==0}).length} minutes read</p>
     <h2 style={{color:props.mode==='light'?'#000000':'#fff'}}>Preview</h2>
     <p style={{color:props.mode==='light'?'#000000':'#fff'}}>{Text}</p>
